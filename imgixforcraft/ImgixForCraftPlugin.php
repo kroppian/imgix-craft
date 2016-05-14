f5c3845abe203a9a2d1f1d1c3b67542d9874572f
Ian Kropp <iankropp@gmail.com>
2016-05-14 15:23:44 -0400

    Fixed a bug in the ImgixForCraftPlugin.php. The class name was improperly written

--- imgixforcraft/ImgixForCraftPlugin.php
+++ imgixforcraft/ImgixForCraftPlugin.php
@@ -1,77 +1,29 @@
 <?php
 namespace Craft;
 
-class FruitAviaryImageEditorPlugin extends BasePlugin
+class ImgixForCraftPlugin extends BasePlugin
 {
     public function getName()
     {
          return Craft::t('Imgix For Craft');
     }
 
-    #public function addAssetActions()
-    #{
-    #    return array('FruitAviaryImageEditor_EditImage');
-    #}
-
-    function getVersion()
+    public function getVersion()
     {
         return '0.0.1';
     }
 
-    function getDeveloper()
+    public function getDeveloper()
     {
         return 'Ian Kropp';
     }
 
-    function getDeveloperUrl()
+    public function getDeveloperUrl()
     {
         return 'https://github.com/kroppian';
     }
-
-    #protected function getSettingsModel()
-    #{
-    #    return new FruitAviaryImageEditor_SettingsModel();
-    #}
-
-    #public function getSettingsHtml()
-    #{
-    #    return craft()->templates->render('fruitaviaryimageeditor/settings', array(
-    #        'settings' => $this->getSettings(),
-    #        'aviaryTools' => craft()->config->get('aviaryTools', 'fruitaviaryimageeditor'),
-    #    ));
-    #}
-
-    public function init() 
+    public function hasCpSection()
     {
-
-
-#        if (craft()->request->isCpRequest()) {
-#            craft()->templates->includeJsResource('fruitaviaryimageeditor/js/editor.js');
-#            $settings = $this->getSettings();
-#            $aviaryTools = is_array($settings['aviaryTools']) ? '[\''.implode ("', '", $settings['aviaryTools']).'\']' : '\'all\'';
-#            $user = craft()->userSession->user;
-#
-#            $js = "
-#                var Fruit = {
-#                    featherEditor : new Aviary.Feather({
-#                        apiKey: '".$settings['aviaryApiKey']."',
-#                        tools: ".$aviaryTools.",
-#                        enableCORS: '".$settings['enableCORS']."' ? true : false,
-#                        theme: '".$settings['aviaryTheme']."',
-#                        language: '".substr($user['preferredLocale'], 0, 2)."'
-#                }),
-#
-#                launchEditor : function (id, src, options) {
-#                    var defaults = {
-#                        image: id,
-#                        url: src
-#                    }
-#                    var options = $.extend({}, defaults, options);
-#                    this.featherEditor.launch(options);
-#                    }
-#                };
-#            ";
-#            craft()->templates->includeJs($js);
-#        }
+        return false;
     }
 }
